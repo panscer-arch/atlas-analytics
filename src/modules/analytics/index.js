8b04dbff02b34e3b04262d2206be1f8f15ce5c70
Digitex <digitex@MacBook-Neo-Digitex.local>
2026-05-18 20:19:47 +0300

    Integrate analytics route registry into top-level page

--- src/modules/analytics/index.js
+++ src/modules/analytics/index.js
@@ -1,3 +1,13 @@
 export { default as AnalyticsPage } from "./AnalyticsPage";
 export { default as AnalyticsRestoredPage } from "./AnalyticsRestoredPage";
 export { default as analyticsRoutes } from "./analytics.routes";
+export {
+  analyticsRouteRegistry,
+  buildAnalyticsUrl,
+  getAnalyticsCanonicalHref,
+  getAnalyticsFaqRoute,
+  getAnalyticsPageRoute,
+  getAnalyticsQueryHref,
+  getAnalyticsSectionRoute,
+  parseAnalyticsRouteFromUrl,
+} from "./routeRegistry";
